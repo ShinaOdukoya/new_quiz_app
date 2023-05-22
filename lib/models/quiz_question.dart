c3d3339adfa2d4582acbc702bb427dd971be6436
ShinaOdukoya <shinaodukoya2@gmail.com>
2023-05-22 15:15:13 +0100

    Managed questions index state

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -3,4 +3,11 @@ class QuizQuestion {
   final List<String> answer;
 
   const QuizQuestion(this.text, this.answer);
+
+  List<String> getShuffledAnswers() {
+    final shuffledList = List.of(answer);
+    shuffledList.shuffle();
+
+    return shuffledList;
+  }
 }
