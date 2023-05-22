32475e054a55f29d8622bb38db401940faa97165
ShinaOdukoya <shinaodukoya2@gmail.com>
2023-05-22 14:03:00 +0100

    Mapped answers to List

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -15,33 +16,41 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       //Takes the entire space
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            'The question',
-            style: TextStyle(
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
               color: Colors.white,
             ),
           ),
           const SizedBox(
             height: 20,
           ),
-          AnswerButton(
-            answerText: 'Answer Text 1',
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: 'Answer Text 2',
-            onTap: () {},
-          ),
-          AnswerButton(
-            answerText: 'Answer Text 3',
-            onTap: () {},
-          ),
+          ...currentQuestion.answer.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {});
+          }),
+          // AnswerButton(
+          //   answerText: currentQuestion.answer[0],
+          //   onTap: () {},
+          // ),
+          // AnswerButton(
+          //   answerText: currentQuestion.answer[1],
+          //   onTap: () {},
+          // ),
+          // AnswerButton(
+          //   answerText: currentQuestion.answer[2],
+          //   onTap: () {},
+          // ),
+          // AnswerButton(
+          //   answerText: currentQuestion.answer[3],
+          //   onTap: () {},
+          // ),
         ],
       ),
     );
